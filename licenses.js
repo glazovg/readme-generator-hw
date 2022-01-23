1487e5f5df13b8e5979ff3897cc26c6bad2c600e
Guillermo Lazo <glazovg@gmail.com>
2022-01-22 23:46:21 -0600

    Adding license.js and readme.js files

---
+++ licenses.js
@@ -0,0 +1,66 @@
+const licenses = {
+    apache: {
+        license: 'Apache 2.0 License',
+        badge: `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    },
+    boost: {
+        license: 'Boost Software License 1.0',
+        badge: `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+    },
+    bsd3: {
+        license: 'BSD 3-Clause License',
+        badge: `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`,
+    },
+    bsd2: {
+        license: 'BSD 2-Clause License',
+        badge: `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+    },
+    eclipse: {
+        license: 'Eclipse Public License 1.0',
+        badge: `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+    },
+    gnuGplv3: {
+        license: 'GNU GPL v3',
+        badge: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    },
+    gnuGlpv2: {
+        license: 'GNU GPL v2',
+        badge: `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+    },
+    gnuAgpl: {
+        license: 'GNU AGPL v3',
+        badge: `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`
+    },
+    gnuLgpl: {
+        license: 'GNU LGPL v3',
+        badge: `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
+    },
+    gnuFdl: {
+        license: 'GNU FDL v1.3',
+        badge: `[![License: FDL 1.3](https://img.shields.io/badge/License-FDL_v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)`
+    },
+    ibm :{
+        license: 'IBM Public License Version 1.0',
+        badge: `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
+    },
+    isc:{
+        license: 'ISC License (ISC)',
+        badge: `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    },
+    mit: {
+        license: 'MIT License',
+        badge: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    },
+    mozilla:{
+        license: 'Mozilla Public License 2.0',
+        badge: `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    },
+    openDataCommons: {
+        license: 'Open Database License (ODbL)',
+        badge: `[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)`
+    },
+    creativeCommons: {
+        license: 'CC0',
+        badge: `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`
+    }
+}
