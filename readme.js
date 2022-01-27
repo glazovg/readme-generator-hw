464a31409ebc19904799fc06704a1c84704ea690
Guillermo Lazo <glazovg@gmail.com>
2022-01-27 11:35:47 -0600

    Adding index.js and main logic

--- readme.js
+++ readme.js
@@ -1,86 +1,59 @@
-const createReadMe = function (projectName, description, installInstructions, usageInformation, contribution, testInstructions, license, licenseBadge, githubUsername, email) {
-    //TODO use fs to create README file
-    
-    `# ${projectName}
-    ${licenseBadge}
+const fs = require('fs');
+const path = require('path');
 
-    ## Description
-    ${description}
+const generateReadMe = function (projectTitle, description, installationInstructions, usageInformation, contribution, testInstructions, license, licenseBadge, githubUsername, email) {
+    license = 
     
-    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+    fs.writeFile(path.join('./result/README.md'),
 
-    - What was your motivation?
-    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    - What problem does it solve?
-    - What did you learn?
+        `
+# ${projectTitle}
+${licenseBadge}
+
+## Description
+${description}
     
-    ## Table of Contents
+## Table of Contents
         
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    
-    ## Installation
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
 
-    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+<a name="installation"></a>
+## Installation
+${installationInstructions}
 
-    ${installInstructions}
+<a name="usage"></a>
+## Usage
+${usageInformation}
     
-    ## Usage
+<a name="license"></a>
+## License
+${license.includes('License') ? `Licensed under the ${license}.` : `Licensed under the ${license} license.`}
+    
+---    
+<a name="how-to-contribute"></a>
+## How to Contribute
+${contribution}
 
-    ${usageInformation}
-    
-    Provide instructions and examples for use. Include screenshots as needed.
-    
-    To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-    "![alt text](assets / images / screenshot.png)"
-    
-    ## Credits
-    
-    List your collaborators, if any, with links to their GitHub profiles.
-    
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    
-    If you followed tutorials, include links to those here as well.
-    
-    ## License
+<a name="tests"></a>
+## Tests
+${testInstructions}
 
-    ${license}
-    
-    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project.
-    
-    ---
-    
-    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-    
-    ## Badges
-    
-    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-    
-    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    
-    ## Features
-    
-    If your project has a lot of features, list them here.
-    
-    ## How to Contribute
+<a name="questions"></a>
+## Questions
+If you have any questions or concerns, please contact me:
 
-    ${contribution}
-    
-    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-    
-    ## Tests
-
-    ${testInstructions}
-    
-    Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-
-    ## Questions
-
-    If you have any questions or concerns, please contact to:
-
-    - ${githubUsername}
-    - ${email}
-    `
+- My github profile https://github.com/${githubUsername}
+- My email ${email}, please with a brief description in the Subject (Bug, Suggestions, Question, Doubts).
+`
+        ,
+        (err) =>
+            err ? console.error(err) : console.log('README file generated!')
+    );
 }
+
+exports.generateReadMe = generateReadMe;
